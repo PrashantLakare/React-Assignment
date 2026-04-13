bb7b657e9977b2c3d2370c95585741b67493363b
Prashant Lakare <Prashant.Lakare@harbingergroup.com>
2026-04-13 15:46:20 +0530

    configured vitest for testing

---
+++ src/setupTests.js
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom/vitest";
+
+if (!window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  });
+}
+
